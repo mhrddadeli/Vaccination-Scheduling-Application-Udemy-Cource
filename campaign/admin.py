ac96cdee5562b426a0caeccd39eb86cef1d9e23e
unknown <RP loptop@mhdd.mhrdd>
2026-05-29 09:21:13 -0700

    Admin Panel Customized

--- campaign/admin.py
+++ campaign/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from campaign.models import Campaign, Slot
 
+class SlotInline(admin.TabularInline):
+    model = Slot
+    readonly_fields = ["reserved"]
 
-admin.site.register(Campaign)
-admin.site.register(Slot)
+
+class CustomCampaignAdmin(admin.ModelAdmin):
+    inlines = [SlotInline]
+    search_fields = ["vaccine__name", "center__name"]
+    list_display = ["vaccine", "center", "start_date", "end_date"]
+    ordering = ["-start_date"]
+    fields = (
+        ("vaccine"),
+        ("center"),
+        ("start_date", "end_date"),
+        ("agents"),
+    )
+
+
+admin.site.register(Campaign, CustomCampaignAdmin)
